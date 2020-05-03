be577f4f2a0823e809a517ff2bbec1dbc33ec8d9
Imed-Eden <ibezahaf@gmail.com>
2020-05-03 16:06:21 +0200

    Create variables.tf

---
+++ Terraform/variables.tf
@@ -0,0 +1,35 @@
+# Global variables that are describing the project, the creator and the client trigram etc.
+variable "trigram" {
+    default = "i"
+}
+
+variable "location" {
+    default = "Central US"
+}
+
+variable "project" {
+    default = "sentiment"
+}
+
+variable "environment" {
+    default = "prd"
+}
+
+variable "creator" {
+    default = "IEB"
+}
+
+variable "description" {
+    default = "default description"
+}
+
+variable "name" {
+    default = "sentiment"
+}
+
+
+variable "tenantid" {
+    default = "X-X-X-X-X"
+}
+
+####################################################################################################
